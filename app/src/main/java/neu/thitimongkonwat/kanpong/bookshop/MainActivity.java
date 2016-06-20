51de0ac09785d54f83dd24ab16d564ce4ff17606
James <Enkanpong1308@gmail.com>
2016-06-20 13:38:21 +0700

    Intent to SignUp

--- app/src/main/java/neu/thitimongkonwat/kanpong/bookshop/MainActivity.java
+++ app/src/main/java/neu/thitimongkonwat/kanpong/bookshop/MainActivity.java
@@ -1,7 +1,9 @@
 package neu.thitimongkonwat.kanpong.bookshop;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    } //Main Method เมธอดหลัก
+
+    public void clickSignUpMain(View view) {
+        startActivity(new Intent(MainActivity.this, SignUpActivity.class));
     }
-}
+
+} //Main Class นี่คือ คลาสหลัก
